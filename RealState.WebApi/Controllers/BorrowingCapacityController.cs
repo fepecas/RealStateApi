aefd5907cc3de7c7d1a0b4b0e9df22f440dfbf3d
Felipe Rayo <fsosorio0@misena.edu.co>
2020-01-29 17:44:57 -0500

    Añadir controller y Manager

--- RealState.WebApi/Controllers/BorrowingCapacityController.cs
+++ RealState.WebApi/Controllers/BorrowingCapacityController.cs
@@ -1,13 +1,29 @@
-﻿using System;
+﻿using RealState.Domain;
+using RealState.Model.BorrowingCapacity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
+using System.Threading;
 using System.Web.Http;
 
 namespace RealState.WebApi.Controllers
 {
     public class BorrowingCapacityController : ApiController
     {
+        public BorrowingCapacityManager _borrowingCapacityManager { get; set; }
+
+        public BorrowingCapacityController()
+        {
+            _borrowingCapacityManager = new BorrowingCapacityManager();
+        }
+
+        [HttpPost]
+        public IHttpActionResult Analyze([FromBody] BorrowingCapacity sheet, CancellationToken cancellationToken = default(CancellationToken))
+        {
+            var response = _borrowingCapacity.Analyze(sheet);
+            return Ok(response);
+        }
     }
 }
